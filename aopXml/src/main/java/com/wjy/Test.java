e983ea3efe06888a008c8eb7b8b2319886d97c50
bozhi <bozhi@suneee.com>
2018-10-19 07:18:01 +0800

    提交

---
+++ aopXml/src/main/java/com/wjy/Test.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (C), 2015-2018, 上海象翌微链有限公司
+ * FileName: Test
+ * Author:   suneee
+ * Date:     2018/10/18 11:14
+ * Description: 测试
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.wjy;
+
+import com.wjy.service.CustomerService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈测试〉
+ *
+ * @author suneee
+ * @create 2018/10/18
+ * @since 1.0.0
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        CustomerService customerService = (CustomerService) context.getBean("c1");
+        customerService.update();
+        customerService.save();
+    }
+}
---
+++ aopXml/src/main/java/com/wjy/config/LogAspect.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C), 2015-2018, 上海象翌微链有限公司
+ * FileName: LogAspect
+ * Author:   suneee
+ * Date:     2018/10/18 11:12
+ * Description: 测试日志切面
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.wjy.config;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈测试日志切面〉
+ *
+ * @author suneee
+ * @create 2018/10/18
+ * @since 1.0.0
+ */
+public class LogAspect {
+
+    public void log() {
+        System.out.println("记录日志");
+    }
+}
